0df0f93dab520eafba7a2d6d0135a98d386765db
Malkavianson <alexneto.best@hotmail.com>
2022-05-16 10:43:36 -0300

    massive update
    Inicio do site da Maria

---
+++ public/js/scripts.js
@@ -0,0 +1,6 @@
+// const hortifrut = ['Abacaxi.jpg','Abobora.jpg','Abobrinha.jpg','Ameixa.jpg','Amora.jpg','Banana.jpg','Batata.jpg','BatataDoce.jpg','Beterraba.jpg','Cacau.jpg','Cenoura.jpg','Inhame.jpg','Jilo.jpg','Laranja.jpg','Lichia.jpg','Maca.jpg','Mamao.jpg','Melancia.jpg','Morangos.jpg','Pera.jpg','Pessego.jpg','Pitaya.jpg','Roma.jpg','Uva.jpg'];
+
+console.log('test')
+
+const hortifrut = document.querySelectorAll('.hortifruti')
+console.log(hortifrut)
